307bedbae49ec7990dcaf8c79481b677747b1aa6
suhaas3 <suhaas3w@gmail.com>
2025-06-30 17:03:00 +0530

    Navbar created

--- react-project/src/Components/Navbar/Navbar.js
+++ react-project/src/Components/Navbar/Navbar.js
@@ -9,8 +9,9 @@ function Navbar() {
         <ul type='none' className="nav-lists"> 
           <li className="logo">Logo</li>
           <input className="search" type="text" placeholder="Type ur need"/>
-          <li className="clubs">Clubs</li>
-          <li className="events">Events</li>
+          <li className="clubs">Courses</li>
+          <li className="events">Webinars</li>
+          <li className="digital-products">Digital Products</li>
           <li className="profile">Profile</li>
         </ul>
       </div>
